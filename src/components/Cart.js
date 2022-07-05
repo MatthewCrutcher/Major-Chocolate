9f7180804f623d9b88071cabd3689f4fbf46af8b
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-07-05 14:53:51 +0800

    display message if items are added to cart or not

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 //IMAGES
-import image2 from "./images/image2.png";
-import line from "./images/cart-line-breaker.png";
+
 import line2 from "./images/total-seperator.png";
 import mastercard from "./images/mastercard.png";
 import visa from "./images/visa.png";
@@ -10,11 +9,7 @@ import visa from "./images/visa.png";
 import "./Cart.css";
 // REDUX TOOLKIT --- REACT-REDUX
 import { useDispatch, useSelector } from "react-redux";
-import {
-  increaseAmount,
-  decreaseAmount,
-  totalCartPrice,
-} from "../app/features/cart/cartSlice";
+import { increaseAmount, decreaseAmount } from "../app/features/cart/cartSlice";
 
 function Cart() {
   const dispatch = useDispatch();
@@ -53,7 +48,13 @@ function Cart() {
       <Navbar className="navbar" />
 
       <div className="cart-container">
-        <div className="item-container">{mappingCart}</div>
+        <div className="item-container">
+          {cart.cart.length === 0 ? (
+            <h3>No Items Added.</h3>
+          ) : (
+            <>{mappingCart}</>
+          )}
+        </div>
         <div className="payment-container">
           <h1>Ready to go?</h1>
           <div className="total-amount-container">
